34ee5c5d8f6158cb17f44fdc30ff63216cfb21fa
YangDR <ydrml@hotmail.com>
2018-10-03 08:10:22 +0800

    修改gradle
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,35 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+	repositories {
+		mavenCentral()
+		jcenter()
+	}
+	dependencies {
+		classpath("org.jetbrains.dokka:dokka-gradle-plugin")
+	}
+}
+
 plugins {
-    kotlin("jvm") version "1.3.0-rc-57"
+	kotlin("jvm") version "1.2.71"
+	id("org.jetbrains.dokka") version "0.9.16"
 }
 
 group = "org.mechdancer"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    compile(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
+	compile(kotlin("stdlib-jdk8"))
+	testCompile("junit", "junit", "+")
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+	sourceCompatibility = JavaVersion.VERSION_1_8
 }
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+	kotlinOptions.jvmTarget = "1.8"
 }
